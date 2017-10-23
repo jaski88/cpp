62e36be242f49439119a44506b9deb39b5a95f47
Rafal Jaskurzynski <ext-rafal.jaskurzynski@here.com>
2017-10-23 15:00:20 +0200

    binary array sorting

---
+++ data_structures_and_algorithms/array.h
@@ -0,0 +1,82 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <string>
+#include <numeric>
+#include <unordered_map>
+#include <algorithm>
+
+class Array
+{
+public:
+    template < typename T >
+    static void
+    print_pairs( T array, int expected )
+    {
+        std::sort( array.begin( ), array.end( ) );
+
+        std::unordered_map< int, int > map;
+
+        for ( auto const i : array )
+        {
+            const int to_find = map[ expected - i ];
+            const bool found = map.find( to_find ) != map.end( );
+
+            if ( found )
+            {
+                std::cout << "print_pair: " << i << "," << to_find << " for " << expected
+                          << std::endl;
+            }
+
+            map[ i ] = i;
+        }
+    }
+
+    template < typename T >
+    static void
+    print_subarray( const T& array, int expected_sum )
+    {
+        std::vector< int > sum;
+
+        for ( auto it = array.begin( ); it < array.end( ); it++ )
+        {
+            for ( auto it2 = it; it2 < array.end( ); it2++ )
+            {
+                sum.push_back( *it2 );
+
+                if ( std::accumulate( sum.begin( ), sum.end( ), 0 ) == expected_sum )
+                {
+                    std::cout << "print_subarray: " << to_str( sum ) << " for " << expected_sum
+                              << std::endl;
+                }
+            }
+
+            sum.clear( );
+        }
+    }
+
+    template < typename T >
+    static void
+    sort_binary_array( T& array )
+    {
+        int zero_elements = std::count( array.begin( ), array.end( ), 0 );
+        for( int i = 0; i < array.size( ); i++ )
+        {
+            const int element = i <= zero_elements - 1 ? 0 : 1;
+            array[ i ] = element;
+        }
+    }
+
+    template < typename T >
+    static std::string
+    to_str( const T& array )
+    {
+        return std::accumulate( std::next( array.begin( ) ), array.end( ),
+                                std::to_string( array.at( 0 ) ), []( const std::string a, int b )
+                                {
+                                    return a + ", " + std::to_string( b );
+                                } );
+    }
+};
+
+#endif  // ARRAY_H
--- data_structures_and_algorithms/main.cpp
+++ data_structures_and_algorithms/main.cpp
@@ -1,57 +1,24 @@
 #include <iostream>
 
+#include "array.h"
+
 #include <array>
-#include <numeric>
-#include <algorithm>
-#include <unordered_map>
+
 
 using namespace std;
-
-template < typename T >
-std::string
-to_string( const T& array )
-{
-    std::string s
-        = std::accumulate( std::next( array.begin( ) ), array.end( ),
-                           std::to_string( array.at( 0 ) ), []( const std::string a, int b )
-                           {
-                               return a + ", " + std::to_string( b );
-                           } );
-    return s;
-}
-
-template < typename T >
-std::pair< int, int >
-find_pair( T array, int expected )
-{
-    std::sort( array.begin( ), array.end( ) );
-
-    std::unordered_map< int, int > map;
-
-    for ( auto const i : array )
-    {
-        const int to_find = map[ expected - i ];
-        const bool found = map.find( to_find ) != map.end( );
-
-        if ( found )
-        {
-            return std::make_pair( i, to_find );
-        }
-
-        map[ i ] = i;
-    }
-
-    return std::make_pair( 0, 0 );
-}
 
 int
 main( int argc, char* argv[] )
 {
-    const std::array< int, 5 > data = {1, 4, 8, 5, 2};
-    std::cout << "data: " << to_string( data ) << std::endl;
+    const std::array< int, 7 > data = {1, 4, 8, 5, 2,3, 2};
+    Array::print_pairs( data, 7 );
 
-    const std::pair< int, int > pair = find_pair( data, 7 );
-    std::cout << "pair: " << std::get< 0 >( pair ) << "," << std::get< 1 >( pair ) << std::endl;
+    const std::array<int, 10 > find_0_subarray = { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
+    Array::print_subarray( find_0_subarray, 0 );
+
+    std::array< int, 7 > binary_array = { 0 , 1 , 0 , 1, 0, 1, 0 };
+    Array::sort_binary_array( binary_array );
+    std::cout << "binary array: " << Array::to_str( binary_array ) << std::endl;
 
     return 0;
 }
